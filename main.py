10fa47ed2d056efc1ed7eece7d95dd757740b0b1
Sizay9192 <sizay9192@example.com>
2026-03-28 16:49:36 +0500

    Мини игра Грудики TEST1

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from os import getenv
 import asyncio
 from aiogram import Bot, Dispatcher, Router, types, filters
 from dotenv import load_dotenv
+import random
+import time
 
 load_dotenv()
 TOKEN = getenv("BOT_TOKEN")
@@ -22,13 +24,18 @@ async def start_command(message:types.Message):
 
 @router.message(filters.Command("help"))
 async def help_command(message:types.Message):
-    await message.answer(
+    await message.answer( "🌐 Обычные команды\n\n"
                          "/grudik - увеличить число грудиков в пакетике\n" 
                          "/grudik_top - посмотреть топ 10\n"
                          "/donat - поддержать автора бота\n"
+                         "/vip - купить VIP, /vip_info - что такое VIP и как работает\n\n"
+                         "🎮 Мини игры!\n\n"
                          "/costi - бросает кубик\n"
-                         "/casino - запускат казино\n"
-                         "Команды будут пополняться в будущем..."
+                         "/bowling - бросает кубик\n"
+                         "/football - бросает кубик\n"
+                         "/basketball - бросает кубик\n"
+                         "/casino - запускат казино\n\n"
+                         "⏰ Команды будут пополняться в будущем..."
                          )
     
 #=================================================================================
@@ -51,7 +58,57 @@ async def costi_command(message:types.Message):
 async def casino_command(message:types.Message):
     await message.answer_dice(emoji="🎰")
 
+@router.message(filters.Command("bowling"))
+async def bowling_command(message:types.Message):
+    await message.answer_dice(emoji="🎳")
+
+@router.message(filters.Command("basketball"))
+async def basketball_command(message:types.Message):
+    await message.answer_dice(emoji="🏀")
+
+@router.message(filters.Command("football"))
+async def football_command(message:types.Message):
+    await message.answer_dice(emoji="⚽")
+
 #==================================================================================
+#============================== VIP(в будущем) ====================================
+
+
+
+#==================================================================================
+#========================================== Мини игра грудики =====================
+grudik_cooldowns = {}
+grudik_values = {}
+
+@router.message(filters.Command("grudik"))
+async def grudik_command(message:types.Message):
+    user_id = message.from_used.id
+    now = time.time()
+    cooldown = 24*60*60
+
+    if user_id in grudik_cooldowns:
+        last_use = grudik_cooldowns[user_id]
+        if now - last_use < cooldown:
+            remaining = int((cooldown - (now-last_use))/3600)
+            await message.answer(f"Подождите {remaining} часов, прежде чем использовать команду повторно!")
+            return
+    change = random.randint(-5, 6)
+    if random.random() <0.01:
+        grudik_values[user_id] = 0
+        await message.answer(f"Ваши Грудики сгорели, теперь их 0 😔\n"
+                             "Шанс на выпадение такого резульатата = 1%, возможно вы везунчик!\n"
+                             )
+    else:
+        grudik_values[user_id] = grudik_values.get(user_id, 0)+change
+        if change >= 0:
+            await message.answer(f"✔️Ваши грудики увеличились на {change}! Теперь у вас: {grudik_values[user_id]}")
+        else:
+            await message.answer(f"❌Ваши грудики уменьшились на {abs(change)}! Теперь у вас: {grudik_values[user_id]}")
+            
+            grudik_cooldowns[user_id] = now
+
+#==================================================================================
+
 
 async def main():
     bot = Bot(token=TOKEN)
